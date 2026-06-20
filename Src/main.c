40e0f79951d5b20a23701b6a43ec51e8f64da828
NamNguyen <nam.nguyen19@sotatek.com>
2026-06-20 17:32:50 +0700

    Log PWM changes to debug terminal

--- Src/main.c
+++ Src/main.c
@@ -2,14 +2,19 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#if defined(DEBUG)
+#include <stdio.h>
+#define log_pwm(pulse_us)  printf("PA8 PWM: %lu us\r\n", (unsigned long)(pulse_us))
+#else
+#define log_pwm(pulse_us)  ((void)(pulse_us))
+#endif
+
 #define LEDS    (GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15)
 #define PWM_PERIOD_US    20000U
 #define PWM_STEP_MS      1000U
 #define PWM_STEPS        3U
-#define LOG_TIMEOUT_MS   20U
 
 static TIM_HandleTypeDef pwm_timer;
-static UART_HandleTypeDef log_uart;
 
 static void led_init(void)
 {
@@ -34,37 +39,6 @@ static void blink_task(void *argument)
     {
         HAL_GPIO_TogglePin(GPIOD, LEDS);
         vTaskDelay(pdMS_TO_TICKS(500U));
-    }
-}
-
-static void uart_init(void)
-{
-    GPIO_InitTypeDef gpio = {0};
-
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_USART2_CLK_ENABLE();
-
-    gpio.Pin = GPIO_PIN_2;
-    gpio.Mode = GPIO_MODE_AF_PP;
-    gpio.Pull = GPIO_NOPULL;
-    gpio.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    gpio.Alternate = GPIO_AF7_USART2;
-    HAL_GPIO_Init(GPIOA, &gpio);
-
-    log_uart.Instance = USART2;
-    log_uart.Init.BaudRate = 115200U;
-    log_uart.Init.WordLength = UART_WORDLENGTH_8B;
-    log_uart.Init.StopBits = UART_STOPBITS_1;
-    log_uart.Init.Parity = UART_PARITY_NONE;
-    log_uart.Init.Mode = UART_MODE_TX;
-    log_uart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    log_uart.Init.OverSampling = UART_OVERSAMPLING_16;
-
-    if (HAL_UART_Init(&log_uart) != HAL_OK)
-    {
-        for (;;)
-        {
-        }
     }
 }
 
@@ -110,11 +84,6 @@ static void pwm_init(void)
 static void pwm_task(void *argument)
 {
     static const uint32_t pulse_us[] = {1000U, 1500U, 2000U};
-    static const uint8_t log_message[][18] = {
-        "PA8 PWM: 1.0 ms\r\n",
-        "PA8 PWM: 1.5 ms\r\n",
-        "PA8 PWM: 2.0 ms\r\n"
-    };
     uint32_t step = 0U;
 
     (void)argument;
@@ -122,8 +91,7 @@ static void pwm_task(void *argument)
     for (;;)
     {
         __HAL_TIM_SET_COMPARE(&pwm_timer, TIM_CHANNEL_1, pulse_us[step]);
-        HAL_UART_Transmit(&log_uart, log_message[step],
-                          sizeof(log_message[step]) - 1U, LOG_TIMEOUT_MS);
+        log_pwm(pulse_us[step]);
         step = (step + 1U) % PWM_STEPS;
         vTaskDelay(pdMS_TO_TICKS(PWM_STEP_MS));
     }
@@ -134,7 +102,6 @@ int main(void)
     HAL_Init();
 
     led_init();
-    uart_init();
     pwm_init();
 
     if (xTaskCreate(blink_task, "blink", 128U, NULL, 1U, NULL) != pdPASS)
@@ -144,7 +111,7 @@ int main(void)
         }
     }
 
-    if (xTaskCreate(pwm_task, "pwm", 128U, NULL, 1U, NULL) != pdPASS)
+    if (xTaskCreate(pwm_task, "pwm", 256U, NULL, 1U, NULL) != pdPASS)
     {
         for (;;)
         {
