aec20ceebaa7b5cef23523a890c2836617f11fd2
NamNguyen <nam.nguyen19@sotatek.com>
2026-06-20 15:26:32 +0700

    Blink all four LEDs in one task

--- Src/main.c
+++ Src/main.c
@@ -3,13 +3,13 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-/* Only the registers needed for the green LED on PD12. */
+/* Only the registers needed for the four LEDs on PD12--PD15. */
 #define RCC_AHB1ENR    ( *( volatile uint32_t * ) 0x40023830U )
 #define GPIOD_MODER    ( *( volatile uint32_t * ) 0x40020C00U )
 #define GPIOD_ODR      ( *( volatile uint32_t * ) 0x40020C14U )
 
 #define GPIOD_CLOCK    ( 1U << 3 )
-#define GREEN_LED      ( 1U << 12 )
+#define LEDS           ( ( 1U << 12 ) | ( 1U << 13 ) | ( 1U << 14 ) | ( 1U << 15 ) )
 
 void SystemInit( void )
 {
@@ -21,11 +21,11 @@ static void led_init( void )
 {
 	RCC_AHB1ENR |= GPIOD_CLOCK;
 
-	/* PD12 = output. */
-	GPIOD_MODER &= ~( 3U << 24 );
-	GPIOD_MODER |= ( 1U << 24 );
+	/* PD12--PD15 = output. */
+	GPIOD_MODER &= ~( ( 3U << 24 ) | ( 3U << 26 ) | ( 3U << 28 ) | ( 3U << 30 ) );
+	GPIOD_MODER |= ( ( 1U << 24 ) | ( 1U << 26 ) | ( 1U << 28 ) | ( 1U << 30 ) );
 
-	GPIOD_ODR &= ~GREEN_LED;
+	GPIOD_ODR &= ~LEDS;
 }
 
 static void blink_task( void * argument )
@@ -34,7 +34,7 @@ static void blink_task( void * argument )
 
 	for( ;; )
 	{
-		GPIOD_ODR ^= GREEN_LED;
+		GPIOD_ODR ^= LEDS;
 		vTaskDelay( pdMS_TO_TICKS( 500U ) );
 	}
 }
